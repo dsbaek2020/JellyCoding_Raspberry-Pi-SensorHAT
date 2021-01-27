5dbeebf90f7a09e7b93bbba658510d9d377f15bf
dsbaek2020 <dsbaek898@icloud.com>
2021-01-27 16:16:24 +0900

    [add speed attribute]

--- raindropUsingClass.py
+++ raindropUsingClass.py
@@ -7,9 +7,10 @@ from random import randint
 sense = SenseHat()
 
 class rain (object):
-    def __init__(self, x, y, r,g,b):
+    def __init__(self, x, y, vy, r,g,b):
         self.x = x
         self.y = y
+        self.vy = vy
         self.rgb = (r,g,b)
     def draw(self):
         '''
@@ -21,19 +22,19 @@ class rain (object):
         for i in range(4):
             y= self.y - i
             if 0<= y <=8:
-                sense.set_pixel(self.x, y, self.rgb)
+                sense.set_pixel(self.x, int(y), self.rgb)
              
     def drop(self):
-        self.y +=1
-        if self.y == 8:
+        self.y +=self.vy
+        if int(self.y) == 8:
             self.restart()    
     def restart(self):
         self.x = randint(0,7)
         self.y = 0
 
-rain1 = rain(0,0,100,100,150)
-rain2 = rain(7,0,150,100,100)
-rain3 = rain(3,0,100,150,100)
+rain1 = rain(0,0,0.5 ,100,100,150)
+rain2 = rain(7,0,0.25,150,100,100)
+rain3 = rain(3,0,1.0 ,100,150,100)
 
 while True:
     sense.clear()
