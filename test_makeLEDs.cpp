3642e1fb776e7d65ec40c43c8eb1b3d30913526e
dsbaek2020 <60423547+dsbaek2020@users.noreply.github.com>
2021-02-22 17:59:47 +0900

    Update test_makeLEDs.cpp

--- test_makeLEDs.cpp
+++ test_makeLEDs.cpp
@@ -1,6 +1,3 @@
-
-//This file modified code for test my raspberry pi, original code:  https://github.com/derekmolloy/exploringrpi/blob/master/chp05/makeLEDOOP/makeLEDs.cpp
-
 /** Simple Sysfs LED control program - written by Derek Molloy
 *    simple OOP  struture for the Exploring Raspberry Pi
 *
@@ -12,75 +9,76 @@
 #include<iostream>
 #include<fstream>
 #include<string>
-#include<unistd.h>
-
+#include<unistd.h>         // for the microsecond sleep function
 using namespace std;
-#define GPIO "/sys/class/gpio/"
-#define FLASH_DELAY 50000
+#define GPIO         "/sys/class/gpio/"
+#define FLASH_DELAY  50000 // 50 milliseconds
 
 class LED{
-	private:
-		string gpioPath;
-		int gpioNumber;
-		void writeSysfs(string path, string filename, string value);
-	public:
-		LED(int gipoNumber);
-		virtual void turnOn();
-		virtual void turnOff();
-		virtual void displayState();
-		virtual ~LED();
+   private:                // the following is part of the implementation
+      string gpioPath;     // private states
+      int    gpioNumber;
+      void writeSysfs(string path, string filename, string value);
+   public:                 // part of the public interface
+      LED(int gpioNumber); // the constructor -- create the object
+      virtual void turnOn();
+      virtual void turnOff();
+      virtual void displayState();
+      virtual ~LED();      // the destructor -- called automatically
 };
 
-LED::LED(int gpioNumber){
-	this->gpioNumber = gpioNumber;
-	gpioPath = string(GPIO "gpio") + to_string(gpioNumber) + string("/");
-	writeSysfs(string(GPIO), "export", to_string(gpioNumber));
-	usleep(100000);
-	writeSysfs(gpioPath, "direction", "out");
+LED::LED(int gpioNumber){  // constructor implementation
+   this->gpioNumber = gpioNumber;
+   gpioPath = string(GPIO "gpio") + to_string(gpioNumber) + string("/");
+   writeSysfs(string(GPIO), "export", to_string(gpioNumber));
+   usleep(100000);         // ensure GPIO is exported
+   writeSysfs(gpioPath, "direction", "out");
 }
 
+// This implementation function is "hidden" outside of the class
 void LED::writeSysfs(string path, string filename, string value){
-	ofstream fs;
-	fs.open((path+filename).c_str());
-	fs << value;
-	fs.close();
+   ofstream fs;
+   fs.open((path+filename).c_str());
+   fs << value;
+   fs.close();
 }
 
 void LED::turnOn(){
-	writeSysfs(gpioPath, "value","1");
+   writeSysfs(gpioPath, "value", "1");
 }
 
 void LED::turnOff(){
-	writeSysfs(gpioPath, "value","0");
+   writeSysfs(gpioPath, "value", "0");
 }
 
 void LED::displayState(){
-	ifstream fs;
-	fs.open((gpioPath + "value").c_str());
-	string line;
-	cout << "The current LED state is";
-	while(getline(fs,line)) cout << line << endl;
-	fs.close();
-}
-LED::~LED(){
-	cout << "Destroying the LED with GPIO number" << gpioNumber << endl;
-	writeSysfs(string(GPIO), "unexport", to_string(gpioNumber));
+   ifstream fs;
+   fs.open((gpioPath + "value").c_str());
+   string line;
+   cout << "The current LED state is ";
+   while(getline(fs,line)) cout << line << endl;
+   fs.close();
 }
 
-int main(int argc, char* argv[]){
-	cout << "Starting the makeLEDs program" << endl;
-	LED led1(4), led2(17);
-	cout << "Flashing the LEDs for 5 seconds" << endl;
-	for(int i=0; i<50; i++){
-		led1.turnOn();
-		led2.turnOff();
-		usleep(FLASH_DELAY);
-		led1.turnOn();
-		led2.turnOff();
-		usleep(FLASH_DELAY);
-	}
-	led1.displayState();
-	led2.displayState();
-	cout << "Finished the makeLEDs program" << endl;
-	return 0;
+LED::~LED(){  // The destructor unexports the sysfs GPIO entries
+   cout << "Destroying the LED with GPIO number " << gpioNumber << endl;
+   writeSysfs(string(GPIO), "unexport", to_string(gpioNumber));
+}
+
+int main(int argc, char* argv[]){  // the main function start point
+   cout << "Starting the makeLEDs program" << endl;
+   LED led1(4), led2(17);          // create two LED objects
+   cout << "Flashing the LEDs for 5 seconds" << endl;
+   for(int i=0; i<50; i++){        // LEDs will alternate
+      led1.turnOn();               // turn GPIO4 on
+      led2.turnOff();              // turn GPIO17 off
+      usleep(FLASH_DELAY);         // sleep for 50ms
+      led1.turnOff();              // turn GPIO4 off
+      led2.turnOn();               // turn GPIO17 on
+      usleep(FLASH_DELAY);         // sleep for 50ms
+   }
+   led1.displayState();            // display final GPIO4 state
+   led2.displayState();            // display final GPIO17 state
+   cout << "Finished the makeLEDs program" << endl;
+   return 0;
 }
