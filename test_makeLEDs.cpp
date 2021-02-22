e7ee029e57c835cf0ea2d60395c5c4f732dc70a6
dsbaek2020 <60423547+dsbaek2020@users.noreply.github.com>
2021-02-22 17:59:02 +0900

    Create test_makeLEDs.cpp

---
+++ test_makeLEDs.cpp
@@ -0,0 +1,86 @@
+
+//This file modified code for test my raspberry pi, original code:  https://github.com/derekmolloy/exploringrpi/blob/master/chp05/makeLEDOOP/makeLEDs.cpp
+
+/** Simple Sysfs LED control program - written by Derek Molloy
+*    simple OOP  struture for the Exploring Raspberry Pi
+*
+*    This program can be used to easliy control multiple LEDS using a class.
+*    This program uses C++11 for the to_string() function and therefore must
+*    be compiled with the -std=c++11 flag.
+*/
+
+#include<iostream>
+#include<fstream>
+#include<string>
+#include<unistd.h>
+
+using namespace std;
+#define GPIO "/sys/class/gpio/"
+#define FLASH_DELAY 50000
+
+class LED{
+	private:
+		string gpioPath;
+		int gpioNumber;
+		void writeSysfs(string path, string filename, string value);
+	public:
+		LED(int gipoNumber);
+		virtual void turnOn();
+		virtual void turnOff();
+		virtual void displayState();
+		virtual ~LED();
+};
+
+LED::LED(int gpioNumber){
+	this->gpioNumber = gpioNumber;
+	gpioPath = string(GPIO "gpio") + to_string(gpioNumber) + string("/");
+	writeSysfs(string(GPIO), "export", to_string(gpioNumber));
+	usleep(100000);
+	writeSysfs(gpioPath, "direction", "out");
+}
+
+void LED::writeSysfs(string path, string filename, string value){
+	ofstream fs;
+	fs.open((path+filename).c_str());
+	fs << value;
+	fs.close();
+}
+
+void LED::turnOn(){
+	writeSysfs(gpioPath, "value","1");
+}
+
+void LED::turnOff(){
+	writeSysfs(gpioPath, "value","0");
+}
+
+void LED::displayState(){
+	ifstream fs;
+	fs.open((gpioPath + "value").c_str());
+	string line;
+	cout << "The current LED state is";
+	while(getline(fs,line)) cout << line << endl;
+	fs.close();
+}
+LED::~LED(){
+	cout << "Destroying the LED with GPIO number" << gpioNumber << endl;
+	writeSysfs(string(GPIO), "unexport", to_string(gpioNumber));
+}
+
+int main(int argc, char* argv[]){
+	cout << "Starting the makeLEDs program" << endl;
+	LED led1(4), led2(17);
+	cout << "Flashing the LEDs for 5 seconds" << endl;
+	for(int i=0; i<50; i++){
+		led1.turnOn();
+		led2.turnOff();
+		usleep(FLASH_DELAY);
+		led1.turnOn();
+		led2.turnOff();
+		usleep(FLASH_DELAY);
+	}
+	led1.displayState();
+	led2.displayState();
+	cout << "Finished the makeLEDs program" << endl;
+	return 0;
+}
