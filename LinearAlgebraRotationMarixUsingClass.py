8231c765c0fe0f5e11e33643dfb2757c6c0b8e01
dsbaek2020 <60423547+dsbaek2020@users.noreply.github.com>
2021-04-07 12:28:52 +0900

    Create LinearAlgebraRotationMarixUsingClass.py

---
+++ LinearAlgebraRotationMarixUsingClass.py
@@ -0,0 +1,96 @@
+
+
+#created by KYL, BDS
+
+import math
+import  numpy as np
+from sense_hat import SenseHat
+from time import sleep
+
+
+sense = SenseHat()
+
+print('hello')
+b = (0, 0, 0)
+r = (80, 0, 0)
+p = (150, 100, 100)
+g = (0,100,0)
+y = (100,100,0)
+
+# 원본 그림 
+figIn = [[b, b, b, b, b, b, b, b], 
+         [b, b, b, b, b, b, b, b],
+         [b, p, r, g, r, r, b, b], 
+         [b, r, r, r, r, r, b, b],
+         [b, r, r, r, y, r, b, b], 
+         [b, r, r, r, r, r, b, b],
+         [b, b, b, b, b, b, b, b], 
+         [b, b, b, b, b, b, b, b]]
+
+class Image(object):
+
+  def __init__(self, Img):
+    self.inputImg = Img
+    self.outImg = [ [b for j in range(8)] for i in range(8)]
+
+  def clearOutImg(self):
+     #clear figOut
+    for i in range(8):   # row index of 2D list.
+        for j in range(8): # column index of 2D list.
+            self.outImg[i][j] = b
+
+  def rotation(self, th):
+
+    self.clearOutImg()
+    for i in range(8):   # row index of 2D list.
+      for j in range(8): # column index of 2D list.
+
+
+        color = self.inputImg[i][j]
+
+        x = j-3
+        y = -i+3
+
+        #print (x,y)
+
+        v = np.array([[x],[y]])
+
+        v1x = math.cos(math.radians(th))
+        v1y = math.sin(math.radians(th))
+        v2x = -1 * math.sin(math.radians(th))
+        v2y = math.cos(math.radians(th))
+
+        M = np.array([[v1x, v1y], [v2x, v2y]])
+
+        v_out = M.dot(v)
+
+        #print (v_out)
+
+        x_o = v_out[0][0]
+        y_o = v_out[1][0]
+
+        #print(x_o,y_o)
+
+        j_o = x_o+3
+        i_o = -y_o+3
+
+        j_o = int(j_o)  
+        i_o = int(i_o)
+
+        if 0 <= j_o <=7 and 0<= i_o <=7:
+          self.outImg[i_o][j_o] = color 
+
+    return self.outImg
+
+
+img1 =  Image(figIn) 
+
+while True:
+  for degree in range(0,360,1):
+    img1.rotation(degree)
+    #sense.set_pixels([pixel for line in figOut for pixel in line])
+    sense.set_pixels(sum(img1.outImg,[]))
+    #sleep(0.01)
+
+
+
