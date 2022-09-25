0b82bff699bd1d90494a858394ae755aa18f3032
dsbaek2020 <60423547+dsbaek2020@users.noreply.github.com>
2022-09-25 12:52:16 +0900

    add joystick event
    조이스틱 제어시 이벤트를 발생하고, 이벤트 핸들러 함수에서 directionkey 변수의 값을 
    조이스틱 방향값으로 바꾼다. 
    Move Slug 함수에서 directionkey변수 값이 지시하는 방향으로 slug의 이동방향을 전환한다.

--- slug.py
+++ slug.py
@@ -1,21 +1,49 @@
+#This file make Slug game, with SenseHat
+#author : Jung En-Jun, Lee Min-Hung
+#advisor: Baek Dae-Seong
+#2022.08. ~ 22.09.24
 from sense_hat import SenseHat
 from time import sleep
 
+#define global variavle---------------
 sense = SenseHat()
-
 slug = [[5,3], [6,3],[7,3]]
-directionkey = 'left'
-
+directionkey = 'up'
 v = [0,0]
+
+#define functions--------------------
+def change_up(event):
+    global directionkey
+    if event.action == 'pressed':
+        print('up')
+        directionkey = 'up'
+        
+def change_down(event):
+    global directionkey
+    if event.action == 'pressed':
+         print('down')
+         directionkey = 'down'
+         #print('____directionkey= ', directionkey)
+    
+def change_right(event):
+    global directionkey
+    if event.action == 'pressed':
+         directionkey = 'right'
+         
+def change_left(event):
+    global directionkey
+    if event.action == 'pressed':
+         directionkey = 'left'
+
 def draw_slug():
   for part in slug:
     x=part[0]
     y=part[1]
     print(x, y)
-    sense.set_pixel(x,y, (100,0,50))
+    sense.set_pixel(x, y, (100,0,50))
     
 def move_slug():
-  global v
+  global v, directionkey
   v[0] = slug[1][0]
   v[1] = slug[1][1]
   slug[2][0] = v[0]
@@ -23,11 +51,14 @@ def move_slug():
 
   v[0] = slug[0][0]
   v[1] = slug[0][1]
-  slug[1][0] = v[0]
+  slug[1][0] = v[0]  
   slug[1][1] = v[1]
 
 
+  print('directionkey= ', directionkey)
+  
   if directionkey == 'down':
+    print('hello')
     slug[0][1] += 1
   elif directionkey == 'up':
     slug[0][1] -= 1
@@ -36,12 +67,13 @@ def move_slug():
   else:
     slug[0][0] += 1
     
+sense.stick.direction_up = change_up
+sense.stick.direction_down = change_down
+sense.stick.direction_left = change_left
+sense.stick.direction_right = change_right
+
 while True:
   sense.clear()
   draw_slug()
   move_slug()
   sleep(0.5)
-
-#while True:
-  #move slug()
-  #sleep(0.2)
