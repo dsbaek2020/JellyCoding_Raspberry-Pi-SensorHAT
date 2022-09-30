8e8a32e668b4107795c3b8fc78967a3d7f13d24b
dsbaek2020 <60423547+dsbaek2020@users.noreply.github.com>
2022-09-30 18:16:23 +0900

    move algorithm update
    리스트 값을 한칸씩 옆으로 이동하는 방식을 
    insert와 pop를 이용해서 구현함 
    주의) insert 할 때는 리스트의 자기자신에 포함됨 원소 위치를 대입하면 값이 원소끼리 연결되어버리고 원소의 값이 같아지는 문제가 있음,
    그래서 리스트에 값을 넣기 위해서(insert, append, = 연산으로 대입)  하는 모든 연산이나 메소드는 
    위치를 대입하면 안되며, 값을 읽어서 해야함 함.

--- slug.py
+++ slug.py
@@ -1,79 +1,77 @@
 #This file make Slug game, with SenseHat
-#author : Jung En-Jun, Lee Min-Hung
+#author : Jung En-Jun, Lee Min-Hung, kim min-ji
 #advisor: Baek Dae-Seong
-#2022.08. ~ 22.09.24
+#2022.08. ~ 22.09.30
 from sense_hat import SenseHat
 from time import sleep
 
-#define global variable---------------
+
 sense = SenseHat()
 slug = [[5,3], [6,3],[7,3]]
-directionkey = 'up'
+directionkey = 'left'
 v = [0,0]
 
-#define functions--------------------
+
 def change_up(event):
     global directionkey
     if event.action == 'pressed':
         print('up')
         directionkey = 'up'
         
+        
 def change_down(event):
-    global directionkey
-    if event.action == 'pressed':
-         print('down')
-         directionkey = 'down'
-         #print('____directionkey= ', directionkey)
-    
+   global directionkey
+   if event.action == 'pressed':
+        print('down')
+        directionkey = 'down'
+        
+        
 def change_right(event):
     global directionkey
     if event.action == 'pressed':
-         directionkey = 'right'
-         
+        directionkey = 'right'
+        
+        
 def change_left(event):
     global directionkey
     if event.action == 'pressed':
-         directionkey = 'left'
-
+        directionkey = 'left'
+        
+        
 def draw_slug():
-  for part in slug:
-    x=part[0]
-    y=part[1]
-    print(x, y)
-    sense.set_pixel(x, y, (100,0,50))
-    
+    for part in slug:
+        x=part[0]
+        y=part[1]
+        sense.set_pixel(x, y, (100,0,50))
+
+
 def move_slug():
-  global v, directionkey
-  v[0] = slug[1][0]
-  v[1] = slug[1][1]
-  slug[2][0] = v[0]
-  slug[2][1] = v[1]
+    global slug, directionkey, k
+    #head = slug[0]
+    head_x = slug[0][0]
+    head_y = slug[0][1]
+   
+    if directionkey == 'down':
+        head_y += 1
+    elif directionkey == 'up':
+        head_y -= 1
+    elif directionkey == 'left':
+        head_x -= 1
+    else:
+        head_x += 1
+               
+    slug.insert(0, [head_x, head_y])
+    k = slug.pop()
 
-  v[0] = slug[0][0]
-  v[1] = slug[0][1]
-  slug[1][0] = v[0]  
-  slug[1][1] = v[1]
-
-
-  print('directionkey= ', directionkey)
-  
-  if directionkey == 'down':
-    print('hello')
-    slug[0][1] += 1
-  elif directionkey == 'up':
-    slug[0][1] -= 1
-  elif directionkey == 'left':
-    slug[0][0] -= 1
-  else:
-    slug[0][0] += 1
-    
+        
 sense.stick.direction_up = change_up
 sense.stick.direction_down = change_down
 sense.stick.direction_left = change_left
 sense.stick.direction_right = change_right
 
 while True:
-  sense.clear()
-  draw_slug()
-  move_slug()
-  sleep(0.5)
+    sense.clear()
+    draw_slug()
+    move_slug()
+    
+    sleep(0.5)
