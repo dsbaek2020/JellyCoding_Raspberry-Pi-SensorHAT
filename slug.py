07b06951c97e0c25ce25ac91ff7c9da0d4f413ca
dsbaek2020 <60423547+dsbaek2020@users.noreply.github.com>
2022-10-07 17:53:25 +0900

    사과 생성 및 점수 증가
    사과 생성, 점수 증가

--- slug.py
+++ slug.py
@@ -1,14 +1,36 @@
 #This file make Slug game, with SenseHat
-#author : Jung En-Jun, Lee Min-Hung, kim min-ji
+#author : Kim Min-seo, Jung En-Jun, Lee Min-Hung, kim min-ji
 #advisor: Baek Dae-Seong
 #2022.08. ~ 22.09.30
 from sense_hat import SenseHat
 from time import sleep
+from random import randint
 
 
 sense = SenseHat()
 slug = [[5,3], [6,3],[7,3]]
 directionkey = 'left'
+score = 0
+appleColor = (243,36,14)
+
+
+def check_meet_apple():
+    slug_x = slug[0][0]
+    slug_y = slug[0][1]
+    
+    if a_x == slug_x and a_y == slug_y:
+      return 'meet'
+    else:
+      return 'noting'
+
+#make draw_apple
+def draw_apple(x,  y):
+  sense.set_pixel(x, y, appleColor )
+
+
+apple_y = (0,7)
+apple_x = (0,7)
+
 
 def change_up(event):
     global directionkey
@@ -51,7 +73,7 @@ def move_slug():
    
     if directionkey == 'down':
         head_y += 1
-    elif directionkey == 'up':
+    elif directionkey == 'up': 
         head_y -= 1
     elif directionkey == 'left':
         head_x -= 1
@@ -67,22 +89,31 @@ def move_slug():
     else:
         return 'I am happy'
 
-
-        
-    
-        
+       
 sense.stick.direction_up = change_up
 sense.stick.direction_down = change_down
 sense.stick.direction_left = change_left
 sense.stick.direction_right = change_right
 
+
+a_x = randint(0, 7)
+a_y = randint(0, 7)
+
 while True:
     sense.clear()
     draw_slug()
+    draw_apple(a_x, a_y)
+    
     status = move_slug()
     if status  == 'game over':
-        sense.show_message('game over')
-        print('game over')
-        break
+      sense.show_message('game over')
+      print('game over')
+      break
     
+    if check_meet_apple() == 'meet':
+      score += 1
+      a_x = randint(0, 7)
+      a_y = randint(0, 7)
+    
+    print(score)
     sleep(0.5)
